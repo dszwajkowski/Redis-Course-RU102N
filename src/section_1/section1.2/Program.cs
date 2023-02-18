7263adb2ee0d7064d32a9f878871826b05956845
Xodvrvp <dawidszwajkowski@gmail.com>
2023-02-18 18:44:24 +0100

    Section 1.2 - connect to and ping Redis db

---
+++ src/section_1/section1.2/Program.cs
@@ -0,0 +1,11 @@
+﻿using StackExchange.Redis;
+
+ConfigurationOptions connectionConfiguration = new()
+{
+    EndPoints = new EndPointCollection { "localhost:6379" },
+};
+var multiplexer = await ConnectionMultiplexer.ConnectAsync(connectionConfiguration);
+var db = multiplexer.GetDatabase();
+
+var pingResult = await db.PingAsync();
+Console.WriteLine($"Ping: {pingResult.TotalMilliseconds} ms");
