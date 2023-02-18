de9667fdd15efd0141c66f1c1618daef5fe86aa5
Xodvrvp <dawidszwajkowski@gmail.com>
2023-02-18 19:40:32 +0100

    Section 2.1 - set and get strings

---
+++ src/section_2/section2.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using StackExchange.Redis;
+using System.Runtime.InteropServices;
+
+ConfigurationOptions connectionConfiguration = new()
+{
+    EndPoints = new EndPointCollection { "localhost:6379" },
+};
+var multiplexer = await ConnectionMultiplexer.ConnectAsync(connectionConfiguration);
+var db = multiplexer.GetDatabase();
+
+var stringKey = new RedisKey("stringKey");
+await db.StringSetAsync(stringKey, "Redis");
+await db.StringAppendAsync(stringKey, " Course");
+var stringFromDb = await db.StringGetAsync(stringKey);
+Console.WriteLine("Full string: " + stringFromDb);
+
+var numericStringKey = new RedisKey("numericStringKey");
+await db.StringSetAsync(numericStringKey, 68);
+await db.StringIncrementAsync(numericStringKey);
+var numericStringFromDb = await db.StringGetAsync(numericStringKey);
+Console.WriteLine("Numeric string: " + numericStringFromDb);
+
+var conditionalKey = new RedisKey("conditionalKey");
+await db.StringSetAsync(conditionalKey, "This string has been set", when: When.NotExists);
+await db.StringSetAsync(conditionalKey, "This string won't be set", when: When.NotExists);
+var conditionalStringFromDb = await db.StringGetAsync(conditionalKey);
+Console.WriteLine(conditionalStringFromDb);
+
+
+
+
