3380aa4432949d29543bf3da3e6068b5e31fd443
Xodvrvp <35074897+Xodvrvp@users.noreply.github.com>
2023-03-04 21:06:10 +0100

    Section 2.8 - transactions

---
+++ src/section_2/section2.8/Program.cs
@@ -0,0 +1,36 @@
+﻿using StackExchange.Redis;
+
+ConfigurationOptions connectionConfiguration = new()
+{
+    EndPoints = new EndPointCollection { "localhost:6379" }
+};
+var multiplexer = await ConnectionMultiplexer.ConnectAsync(connectionConfiguration);
+var db = multiplexer.GetDatabase();
+
+var transaction = db.CreateTransaction();
+
+transaction.HashSetAsync("Person:1", new HashEntry[]
+{
+    new ("name", "Saul"),
+    new ("age", 30),
+    new ("postalCode", "00-001")
+});
+transaction.SortedSetAddAsync("person:name:Saul", "Person:1", 0);
+transaction.SortedSetAddAsync("person:postalCode:00-001", "Person:1", 0);
+transaction.SortedSetAddAsync("person:age", "Person:1", 30);
+bool result = transaction.Execute();
+Console.WriteLine($"Transaction 1 result: {result}.");
+
+// transaction condition
+transaction.AddCondition(Condition.HashEqual("Person:1", "age", 30));
+transaction.HashIncrementAsync("person:1", "age");
+transaction.SortedSetIncrementAsync("person:age", "person:1", 1);
+result = transaction.Execute();
+Console.WriteLine($"Transaction 2 result: {result}.");
+
+// failed transaction condition
+transaction.AddCondition(Condition.HashEqual("person:1", "age", 55));
+transaction.HashIncrementAsync("person:1", "age");
+transaction.SortedSetIncrementAsync("person:age", "person:1", 1);
+result = transaction.Execute();
+Console.WriteLine($"Transaction 3 result: {result}.");
