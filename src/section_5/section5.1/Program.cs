5ff0c29858b29901aa1631297f063de61f788c12
Xodvrvp <35074897+Xodvrvp@users.noreply.github.com>
2023-03-12 16:01:07 +0100

    Section 5.1 - create indexes

---
+++ src/section_5/section5.1/Program.cs
@@ -0,0 +1,9 @@
+﻿using Redis.OM;
+using section5._1;
+
+var provider = new RedisConnectionProvider("redis://localhost:6379");
+
+
+// create indexes
+await provider.Connection.CreateIndexAsync(typeof(Sale));
+await provider.Connection.CreateIndexAsync(typeof(Employee));
