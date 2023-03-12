4777d18d8b0c7c35d21e6292fb3d771011b0f0c5
Xodvrvp <35074897+Xodvrvp@users.noreply.github.com>
2023-03-12 21:25:32 +0100

    Section 5.4 - updating and deleting documents

---
+++ src/section_5/section5.4/Employee.cs
@@ -0,0 +1,20 @@
+﻿using Redis.OM.Modeling;
+
+namespace section5._4;
+
+[Document(StorageType = StorageType.Json, Prefixes = new[] { "Employee" }, IndexName = "employees")]
+public class Employee
+{
+    [RedisIdField]
+    [Indexed]
+    public string? Id { get; set; }
+    [Indexed]
+    public List<string>? Sales { get; set; }
+    [Indexed(JsonPath = "$.Location")]
+    [Indexed(JsonPath = "$.PostalCode")]
+    public Address? Address { get; set; }
+    [Indexed(Sortable = true)]
+    public string? Name { get; set; }
+    [Indexed(Sortable = true)]
+    public int Age { get; set; } 
+}
---
+++ src/section_5/section5.4/Sale.cs
@@ -0,0 +1,17 @@
+﻿using Redis.OM.Modeling;
+
+namespace section5._4;
+
+[Document(StorageType = StorageType.Json)]
+public class Sale
+{
+    [RedisIdField]
+    [Indexed]
+    public string? Id { get; set; }
+    [Indexed]
+    public string? EmployeeId { get; set; }
+    [Indexed]
+    public int Total { get; set; }
+    [Indexed(CascadeDepth = 2)]
+    public Address? Address { get; set; }
+}
